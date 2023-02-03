7ac6e39afdc11c2af15b91c9ce1dc39e898c5f6f
Stefano Ceschi <stefano.ceschi@samtrevanoc.ch>
2023-02-03 23:03:16 +0100

    multiplayer-upd_03.02.23_SC

---
+++ 5_Sito_o_applicativo/Unity/MultiplayerTest/Assets/Assets/Delete.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Networking;
+
+public class Delete : NetworkBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            
+        }
+    }
+
+}
---
+++ 5_Sito_o_applicativo/Unity/MultiplayerTest/Assets/Assets/NetworkCommandLine.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using System;
+using Unity.Netcode.Transports.UTP;
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
+
+public class NetworkCommandLine : MonoBehaviour
+{
+
+    public TextMeshProUGUI textIP;
+
+    private NetworkManager netManager;
+    private UnityTransport unityTransport;
+
+    void Start()
+    {
+        netManager = GetComponentInParent<NetworkManager>();
+        unityTransport = GetComponentInParent<UnityTransport>();
+        textIP.text = "IP: " + GetLocalIPAddress();
+        if (Application.isEditor) return;
+        
+        var args = GetCommandlineArgs();
+
+        if (args.TryGetValue("-mode", out string mode))
+        {
+            switch (mode)
+            {
+                case "server":
+                    netManager.StartServer();
+                    textIP.text += " (SRV)";
+                    break;
+                case "host":
+                    netManager.StartHost();
+                    break;
+                case "client":
+                    textIP.text += " (CLI)";
+                    netManager.StartClient();
+                    break;
+            }
+        }
+    }
+
+    private Dictionary<string, string> GetCommandlineArgs()
+    {
+        Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+        var args = System.Environment.GetCommandLineArgs();
+
+        for (int i = 0; i < args.Length; ++i)
+        {
+            var arg = args[i].ToLower();
+            if (arg.StartsWith("-"))
+            {
+                var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+                value = (value?.StartsWith("-") ?? false) ? null : value;
+
+                argDictionary.Add(arg, value);
+            }
+        }
+        return argDictionary;
+    }
+
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+}
